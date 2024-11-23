d1d52196a773fc91b3b87b8fa73a1948c4e69b8a
chetanr250 <1ds22ai010@dsce.edu.com>
2024-11-24 03:12:25 +0530

    add xml to json

--- backend/main.go
+++ backend/main.go
@@ -1,14 +1,12 @@
 package main
 
 import (
-	// "github.com/alfiyafatima09/NextGoGen/backend/config"
-	// "encoding/xml"
-	// "fmt"
-
 	"fmt"
+	"strings"
 
+	"github.com/alfiyafatima09/NextGoGen/backend/processor/csv"
 	"github.com/alfiyafatima09/NextGoGen/backend/processor/xml"
-	// "github.com/gofiber/fiber/v2"
+
 	"gofr.dev/pkg/gofr"
 )
 
@@ -24,47 +22,45 @@ type ConversionResponse struct {
 func handleConversion(ctx *gofr.Context) (interface{}, error) {
 	var req ConversionRequest
 	if err := ctx.Bind(&req); err != nil {
-		return nil, fmt.Errorf("invalid request format")
+		return nil, fmt.Errorf("invalid request format: %v", err)
+	}
+	path := req.FilePath
+
+	fmt.Println(path)
+	fmt.Println(strings.HasSuffix(path, ".xml"))
+	if strings.HasSuffix(path, ".xml") || true {
+		x, _ := xml.NewXMLToJSONConverter().ConvertFile(req.FilePath)
+		fmt.Println(x)
+	}
+	if strings.HasSuffix(path, ".csv") {
+		return csv.ConvertCSVToJSON(req.FilePath)
 	}
 
+	// x, _ := csv.ConvertCSVToJSON(req.FilePath)
+	// return string(x), nil
+	// return csv.ConvertCSVToJSON(req.FilePath)
 	// Create converter instance
-	xmlConverter := xml.NewXMLToJSONConverter()
+	// xmlConverter := cs.NewXMLToJSONConverter()
 
 	// Validate input file
-	if err := xmlConverter.ValidateXMLFile(req.FilePath); err != nil {
-		return nil, err
-	}
+	// if err := xmlConverter.ValidateXMLFile(req.FilePath); err != nil {
+	// 	return nil, err
+	// }
 
-	// Convert file
-	outputPath, err := xmlConverter.ConvertFile(req.FilePath)
-	if err != nil {
-		return nil, err
-	}
+	// outputPath, err := xmlConverter.ConvertFile(req.FilePath)
+	// if err != nil {
+	// return nil, err
+	// }
 
 	return ConversionResponse{
-		OutputPath: outputPath,
+		OutputPath: "outputPath",
 		Message:    "File converted successfully",
 	}, nil
 }
 
 func main() {
 	app := gofr.New()
-	app.POST("/xml-json", handleConversion)
+	app.POST("/toJson", handleConversion)
 	app.Run()
-	// fmt.Print(abc)
 
-	// cfg := config.LoadConfig()
-
-	// Initialize Kafka
-	// producer := kafka.NewProducer(cfg.KafkaBrokers)
-	// consumer := kafka.NewConsumer(cfg.KafkaBrokers)
-
-	// go processor.ProcessData(consumer, producer, cfg.RawTopic, cfg.ProcessedTopic)
-
-	// Initialize Fiber app
-	// app := fiber.New()
-	// routes.SetupRoutes(app, cfg, producer)
-
-	// Start server
-	// app.Listen(":3000")
 }
